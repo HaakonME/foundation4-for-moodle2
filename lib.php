d2a2f2f84da07f42e95358fb993947d1e15d7f0c
Danny Wahl <danny@iywahl.com>
2013-04-09 13:49:20 +0800

    fix incorrect function name

--- lib.php
+++ lib.php
@@ -27,7 +27,7 @@
 require_once(dirname(__FILE__).'/lib/css_postprocessor.php');
 
 // Load Moodle jQuery
-function theme_sometheme_page_init(moodle_page $page) {
+function theme_foundation_page_init(moodle_page $page) {
     $page->requires->jquery();
 }
 ?>
